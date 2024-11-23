6b244e9a35a1c0b7200a7d44596e30a7e5269eea
palian <cpalianyu@tutamail.com>
2024-11-22 21:21:02 -0500

    agregamos fpdf

--- Problema_1/index.php
+++ Problema_1/index.php
@@ -1,5 +1,9 @@
 <?php
 
-echo "hola mundo";
-
+require('fpdf.php');
+$pdf = new FPDF();
+$pdf->AddPage();
+$pdf->SetFont('Arial','B',16);
+$pdf->Cell(40,10,utf8_decode('Hola, Mundo'));
+$pdf->Output();
 ?>
